357c536555152f631b3f6ae9d6dab5a503fe1e8e
Anton <afrantisak05@gmail.com>
2014-09-07 18:39:56 -0500

    first try

---
+++ pokemoncard.py
@@ -0,0 +1,14 @@
+name = raw_input("what is your name ")
+health = raw_input("health ")
+health = int(health)
+while True:
+    damage = raw_input("damage ")
+    damage = int(damage)
+    weakness = raw_input("weakness ")
+    weakness = int(weakness)
+    real_damage = weakness * damage
+    resistance = raw_input("resistance ")
+    resistance = int(resistance)
+    correct_damage = real_damage + resistance
+    health = health - correct_damage 
+    print "{name}, health left {difference}".format(name=name, difference = health)
